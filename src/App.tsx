1cac2420d471cdcc9f48a39afb3c5bc83baaddab
Pankaj-0308 <pankajkansal1808@gmail.com>
2025-07-12 16:18:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import VirtualTryOn from "./components/VirtualTryOn";
 import Footer from "./components/Footer";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 const App = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <VirtualTryOn />
-      <Footer />
-    </div>
+    <ErrorBoundary>
+      <div className="min-h-screen flex flex-col">
+        <VirtualTryOn />
+        <Footer />
+      </div>
+    </ErrorBoundary>
   );
 };
 
